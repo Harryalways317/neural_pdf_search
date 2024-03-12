b3c42d9372d6974378f64a51c7747893c156e8b2
Harish Vadaparty <harishvadaparty@gmail.com>
2024-03-12 12:53:01 +0530

    Update neural_searcher.py

--- neural_search/neural_searcher.py
+++ neural_search/neural_searcher.py
@@ -2,12 +2,22 @@ import time
 from typing import List
 from sentence_transformers import SentenceTransformer
 from qdrant_client import QdrantClient
-from qdrant_client.http.models.models import Filter, SearchParams
+from qdrant_client.http.models.models import Filter, SearchParams, FieldCondition, Match
 from fuzzywuzzy import process
 from config import QDRANT_URL, QDRANT_API_KEY, EMBEDDINGS_MODEL
+from qdrant_client.http import models
 
+
+def normalize_text(text):
+    """Normalize text by making it lower case and removing spaces."""
+    return text.lower().replace(" ", "")
+
+def is_partial_match(query, candidate):
+    """Check if query is a partial match of the candidate."""
+    return query in candidate or candidate in query
 
 class NeuralSearcher:
+
 
     def __init__(self, collection_name: str):
         self.collection_name = collection_name
@@ -25,17 +35,21 @@ class NeuralSearcher:
         )
         print(f"Search took {time.time() - start_time} seconds")
         if ticker is not None:
+            normalized_ticker = normalize_text(ticker) if ticker else None
             filtered_hits = []
             for hit in hits:
                 tickers = hit.metadata.get('tickers', [])
-                # Using fuzzywuzzy to find the best match and its score
-                best_match, score = process.extractOne(ticker, tickers)
-                # 80% threshold for accepting a match
-                if score >= 80: 
+                normalized_tickers = [normalize_text(t) for t in tickers]
+                if ticker:
+                    # Check for partial match between normalized ticker and any of the normalized tickers
+                    if any(is_partial_match(normalized_ticker, nt) for nt in normalized_tickers):
+                        filtered_hits.append(hit.metadata)
+                else:
                     filtered_hits.append(hit.metadata)
             return filtered_hits
         else:
             return [hit.metadata for hit in hits]
+        # return [hit.metadata for hit in hits]
 
     # def search_with_vector(self, texts: list[str], filter_: dict = None, limit: int = 100) -> List[dict]:
     #     # Generate vectors for each text in the list
